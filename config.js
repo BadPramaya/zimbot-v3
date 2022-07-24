b13e31e04c0888a6e6e0ecabe06fd04be9401cd0
x_pramaya_x <78118894+BadPramaya@users.noreply.github.com>
2022-07-24 12:13:02 +0530

    Update config.js
    Signed-off-by: x_pramaya_x <78118894+BadPramaya@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
+global.owner = ['94770611075','']
+global.premium = ['94770611075 ']
 global.botname = 'ZIM BOT INC'
 global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.group1 = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.group2 = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
+global.packname = 'ʜᴇʟʟ ʙᴏʏ ©️x_ᴘʀᴀᴍᴀʏᴀ_x'
+global.ytchannel = 'https://chat.whatsapp.com/Go9cBxSneUpGDbl6gKdF7E'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
@@ -65,7 +65,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = 'ZIM BOT INC'
+global.author = '©️x_ᴘʀᴀᴍᴀʏᴀ_x'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
